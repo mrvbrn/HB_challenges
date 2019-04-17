bd725d27b552acdb8826228cfb1d66dd4942296c
vagrant <vagrant@vagrant.vm>
2019-04-17 19:02:08 +0000

    hexconvert.py is added

--- hard/hexconvert.py
+++ hard/hexconvert.py
@@ -16,6 +16,21 @@
 
 def hex_convert(hex_in):
     """Convert a hexadecimal string, like '1A', into it's decimal equivalent."""
+    decimal_alphabet = {"A" : 10, "B" : 11, "C" : 12, "D" : 13, "E" : 14, "F" : 15}
+    decimal_number = "0123456789"
+
+    hex_in_list = list(hex_in)
+    hex_in_list.reverse()
+    n = 0
+    total = 0
+    while n < len(hex_in):
+        if hex_in_list[n] in decimal_number:
+            total += 16 ** n * int(hex_in_list[n])
+        else:
+            total += 16 ** n * decimal_alphabet[hex_in_list[n]]
+        n += 1
+    return total
+
 
 
 if __name__ == "__main__":
